39bbda88f66c790729464ccc8408b6c64ec4b663
Quentin Durantay <quentin.durantay@gmail.com>
2018-12-22 16:19:17 +0100

    Change text

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
             <div class="container">
                 <div class="columns">
                     <div class="column">
-                        <h2 class="title is-4">👋 I'm Quentin!</h2>
+                        <h2 class="title is-4">👋 Hey I'm Quentin!</h2>
                         <h1 class="title is-3">
-                            I'm a Product Manager and Growth Engineer
+                            A Product Manager and Growth Engineer working
                             <a
                                 href="https://www.dolead.com/?utm_source=Quentin"
                                 target="_blank"
