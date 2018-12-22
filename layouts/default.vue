eceb97afca50fca97d0f964225fb9310ed62efcf
Quentin Durantay <quentin.durantay@gmail.com>
2018-12-22 14:55:48 +0100

    Add email and fontawesome

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,12 @@ export default {
                     rel: "stylesheet",
                     href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css",
                 },
+                {
+                    ref: "stylesheet",
+                    href: "https://use.fontawesome.com/releases/v5.6.3/css/all.css",
+                    integrity: "sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/",
+                    crossorigin: "anonymous",
+                }
             ],
             title: "Quentin Durantay",
         };
@@ -20,10 +26,11 @@ export default {
 
 
 <style lang="scss">
+@import "~bulma/sass/utilities/_all";
+
 @import url('https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,800');
 $family-sans-serif: "Nunito", sans-serif;
 
-@import "~bulma/sass/utilities/_all";
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,49 @@
                             <a
                                 href="https://www.dolead.com/?utm_source=Quentin"
                                 target="_blank"
-                                @mouseover="animate"
-                                :class="{ 'animated': isAnimated, 'jello': isAnimated }"
+                                @mouseover="animate('job')"
+                                :class="{ 'animated': isAnimated.job, 'jello': isAnimated.job }"
                             >
                                 @ Dolead
                             </a>
                         </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hero-foot">
+            <div class="container">
+                <div class="columns is-multiline">
+                    <div class="column is-12">
+                        <p>
+                            Contact me at
+                            <a
+                                href="mailto:quentin@quentindurantay.me"
+                                @mouseover="animate('mail')"
+                                :class="{ 'animated': isAnimated.mail, 'shake': isAnimated.mail }"
+                            >
+                                quentin@quentindurantay.me
+                            </a>
+                        </p>
+                    </div>
+                    <div class="column is-12">
+                        <div class="is-flex">
+                            <div>
+                                <span class="icon">
+                                    <i class="fab fa-github-alt"></i>
+                                </span>
+                            </div>
+                            <div>
+                                <figure class="image is-32x32">
+                                    <img src="" alt="">
+                                </figure>
+                            </div>
+                            <div>
+                                <figure class="image is-32x32">
+                                    <img src="" alt="">
+                                </figure>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -27,13 +64,16 @@
 export default {
     data() {
         return {
-            isAnimated: false,
+            isAnimated: {
+                job: false,
+                mail: false,
+            },
         };
     },
     methods: {
-        animate() {
-            this.isAnimated = true;
-            setTimeout(() => this.isAnimated = false, 500);
+        animate(_obj) {
+            this.isAnimated[_obj] = true;
+            setTimeout(() => this.isAnimated[_obj] = false, 500);
         },
     },
 };
@@ -43,5 +83,17 @@ export default {
 <style lang="scss" scoped>
 a {
     display: inline-block;
+}
+.hero-foot {
+    margin-bottom: 36px;
+}
+.is-flex {
+    align-items: center;
+
+    div {
+        &:not(:first-child) {
+            margin-left: 26px;
+        }
+    }
 }
 </style>
